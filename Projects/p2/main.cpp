7707e633de85a252c490ebb821c1880b353a8a6b
Carlos Manuel Sequí Sánchez <cmss_96@hotmail.com>
2019-10-21 14:28:54 +0200

    exercise 3 done

--- Projects/p2/main.cpp
+++ Projects/p2/main.cpp
@@ -1,6 +1,8 @@
 #include <fplus/fplus.hpp>
 
 typedef std::pair<float, float> point;
+typedef std::pair<point, point> edge;
+typedef std::vector<point> points;
 
 using namespace std;
 
@@ -18,6 +20,29 @@ float point_distance(const point& p1, const point& p2)
 	const float dy = p2.second - p1.second;
 	return std::sqrt(dx * dx + dy * dy);
 }
+
+template <typename Cont>
+Cont product(const Cont & nums)
+{
+	return fplus::reduce(std::multiplies<double>(), 1, nums);
+}
+
+template <typename Cont>
+typename Cont::value_type sum(const Cont& xs)
+{
+	return fplus::reduce(std::plus<int>(), 1, xs);
+}
+
+vector<edge> get_edges(vector<point> polygon)
+{
+	return fplus::overlapping_pairs_cyclic(polygon);
+}
+
+float edge_length(const edge& e)
+{
+	return  point_distance(e.first, e.second);
+}
+
 
 int main() {
 
@@ -37,13 +62,13 @@ int main() {
 //     using point_distance internally,
 //     so the code compiles and runs as intended.
 
-	vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
+	/*vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
 	const auto edges = fplus::overlapping_pairs_cyclic(polygon);
 	const auto calculatePointDistance = [](auto pair) {
 		return point_distance(pair.first, pair.second);
 	};
 	const auto result = fplus::maximum_on( calculatePointDistance, edges);
-	cout << fplus::show(result) << endl;
+	cout << fplus::show(result) << endl;*/
 
 	// otra solucion:
 	/*
@@ -56,7 +81,13 @@ int main() {
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	// EXERCISE 3
-
+//     Implement edge_length and get_edges functions
+//     so the improved code from below works.
+	vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
+	const auto result = fplus::maximum_on(
+		edge_length,
+		get_edges(polygon));
+	cout << fplus::show(result) << endl; 
 }
 
 
