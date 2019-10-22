5323cc4a62fb0412fd2b9d4d502dec99f959e0d7
Carlos Manuel Sequí Sánchez <cmss_96@hotmail.com>
2019-10-22 15:10:38 +0200

    class 11

--- Projects/p2/main.cpp
+++ Projects/p2/main.cpp
@@ -1,4 +1,5 @@
 #include <fplus/fplus.hpp>
+#include <cmath>
 
 typedef std::pair<float, float> point;
 typedef std::pair<point, point> edge;
@@ -73,21 +74,95 @@ int main() {
 	// otra solucion:
 	/*
 	const auto result = fplus::maximum_on(
-        [](const std::pair<point, point>& edge) -> float
-        {
-            return point_distance(edge.first, edge.second);
-        }, edges);
+		[](const std::pair<point, point>& edge) -> float
+		{
+			return point_distance(edge.first, edge.second);
+		}, edges);
 	*/
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	// EXERCISE 3
 //     Implement edge_length and get_edges functions
 //     so the improved code from below works.
-	vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
+	/*vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
 	const auto result = fplus::maximum_on(
 		edge_length,
 		get_edges(polygon));
-	cout << fplus::show(result) << endl; 
+	cout << fplus::show(result) << endl; */
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// EXERCISE 4
+	/*std::vector<std::vector<int>> xss = { {0,1,2}, {3,4,5} };*/
+
+	// Exercise a:
+//     Square every inner value of xss, so
+//     {{0,1,2}, {3,4,5}}
+//     turns into
+//     {{0,1,4}, {9,16,25}}
+//     Try out different possibilities to achieve your goal.
+
+	// el transform externo recibe una funcion y un contenedor.
+	// el transform externo es de fwd (forward), por lo que toma el contenedor
+	// del transform externo y se lo aplica a la funcion que recibe, el square.
+	// hay dos transform debido a que el contenedor es un vector de vectores.
+	// el square tiene un <int> siguiendo la notación std::function<return(parameter)>
+	/*fplus::transform(fplus::fwd::transform(fplus::square<int>), xss);*/
+
+	// Exercise b:
+	//     Use nested lambda expressions
+	//     to make a fully curried version of add_four.
+	//     add_four (Int, Int, Int, Int) -> Int
+	//     add_four_curried : Int -> Int -> Int -> Int -> Int
+	//     so that
+	//     add_four(1,2,3,4)
+	//     means the same as
+	//     add_four_curried(1)(2)(3)(4)
+
+	/*const auto add_four_curried = [](int a)
+	{
+		return [a](int b)
+		{
+			return [a, b](int c)
+			{
+				return [a, b, c](int d)
+				{
+					return (a + b + c + d);
+				};
+			};
+		};
+	};
+
+	add_four_curried(1)(2)(3)(4);*/
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+	// EXERCISE 5
+	vector<point> polygon = { {1,2}, {7,3}, {6,5}, {4,4}, {2,9} };
+
+	/*const auto result = fplus::maximum_on(
+		edge_length,
+		get_edges(polygon));*/
+
+	// exercise a
+	//     Rewrite the the longest-edge code
+//     in forward-application style.
+	const auto result = fplus::fwd::apply(polygon,
+		get_edges,
+		fplus::fwd::maximum_on(edge_length));
+
+	// exercise b
+	//     Invent a quite long chain of function applications,
+//     at least 5 functions,
+//     and implement its application to an input value
+//     in all three styles:
+//         1) Assign intermediate values to variables.
+//         2) Nested function calls
+//         3) Foward-application style
+//     Compare all three versions and contemplate
+//     the readability of all of them.
+
+	cout << fplus::show(result) << endl;
 }
 
 
